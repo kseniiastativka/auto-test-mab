e2f69b8d5681fceb40cd394fa8b77328ba8d0293
Kseniia Stativka <kseniia.stativka@gmail.com>
2018-02-18 22:20:24 +0200

    Add DataProvider to HomeTest, makeInternalPaymentInP2PBlock

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,27 +1,35 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage {
     private WebDriver driver;
 
-    private By allOperations
+    private By allOperationsLocator
             = By.xpath("//div[@class='row thick']//h2");
-    private By sourceAlfaCart
+    private By sourceAlfaCartLocator
             = By.xpath("//div[contains(@class, 'sourceAlfa-P2PTransfer-4rNr47')]/div[contains(@class, 'alfa-P2PCard-1_qKIb')]");
-    private By targetAlfaCart
+    private By targetAlfaCardLocator
             = By.xpath("//div[contains(@class, 'targetAlfa-P2PTransfer-2tVSLX')]/div[contains(@class, 'alfa-P2PCard-1_qKIb')]");
-    private By inputSum = By.className("sumField-P2PTransfer-2MLKLw");
-    private By submitButton = By.className("button-P2PTransfer-2_1bvm");
-    private By targetCardsSelect = By.className("select-P2PCardSelect-2M3uEo");
-    private By targetUsersCardOption = By.xpath("//div[contains(@class, 'targetAlfa-P2PTransfer-2tVSLX')]//ul/li[2]");
+    private By inputSumLocator = By.className("sumField-P2PTransfer-2MLKLw");
+    private By submitButtonLocator = By.className("button-P2PTransfer-2_1bvm");
+    private By targetCardsSelectLocator = By.className("select-P2PCardSelect-2M3uEo");
+    private By targetUsersCardOptionLocator = By.xpath("//div[contains(@class, 'targetAlfa-P2PTransfer-2tVSLX')]//ul/li[2]");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
     public String getAllOperationsText() {
-        return driver.findElement(allOperations).getText();
+        return driver.findElement(allOperationsLocator).getText();
     }
 
-
+    public PaymentProductTemplatePage makeInternalPaymentInP2PBlock(String sum) {
+        driver.findElement(targetAlfaCardLocator).click();
+        driver.findElement(targetCardsSelectLocator).click();
+        driver.findElement(targetUsersCardOptionLocator).click();
+        driver.findElement(inputSumLocator).sendKeys(sum);
+        driver.findElement(submitButtonLocator).click();
+        return new PaymentProductTemplatePage(driver);
+    }
 }
--- src/test/HomeTest.java
+++ src/test/HomeTest.java
@@ -1,3 +1,19 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class HomeTest extends BaseTest {
 
+    @DataProvider(name = "validAmounts")
+    private Object[][] createValidAmounts() {
+        return new Object[][]{
+                {"1,01"},
+                {"345.99"},
+                {"1.00"},
+                {"2"}
+        };
+    }
+    @Test(dataProvider = "validAmounts")
+    public void testPaymentWithAmount(String sum) {
+
+    }
 }
